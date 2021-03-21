027dfaafcf2950c267458780d3a1a162c9e55e06
Jonathan Agatston <jagatston123@gmail.com>
2021-03-20 20:41:31 -0400

    added changes to the js

--- develop/script.js
+++ develop/script.js
@@ -3,7 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var userChoice = window.prompt("How many characters?");
+  var userChoice = window.prompt("Would you like uppercase? (Y/N?)");
+  var userChoice = window.prompt("Would you like lowercase? (Y/N?)");
+  var userChoice = window.prompt("Would you like numbers? (Y/N?)");
+  var userChoice = window.prompt("Would you like special characters? (Y/N?)");
+  var password = generatePassword(userChoice);
   var passwordText = document.querySelector("#password");
   Math.random().toString(36).substr(2, 8);
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -14,11 +19,13 @@ function writePassword() {
 
 }
 
-
-function generatePassword(){
-  // Write the code that asks the user about the parameters for their password
-  return "Temporary Password";
+function generatePassword(numberOfCharacters) {
+  return numberOfCharacters;
 }
+// function generatePassword(){
+//   // Write the code that asks the user about the parameters for their password
+//   return "Temporary Password";
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
